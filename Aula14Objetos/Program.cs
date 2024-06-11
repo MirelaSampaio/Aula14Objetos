6039d92ecc06357813003a896f8c06aa57f87862
Misa <135743122+MirelaSampaio@users.noreply.github.com>
2024-06-11 19:25:59 -0300

    Update Program.cs

--- Aula14Objetos/Program.cs
+++ Aula14Objetos/Program.cs
@@ -1,10 +1,10 @@
 ﻿using Aula14Objetos;
 
-Pessoa p1 = new Pessoa("Misa", 19, "Atilio Pratavieira", "Aracy I", "São Carlos");
+Pessoa p1 = new Pessoa("Misa", 19, "Rua dos bobos, nº 0", "Engracinho", "São Juju");
 
 // Para relacionar o endereço a classe pessoa, é necessário seguir esse procedimento:
 
-//Endereco e1 = new Endereco("Atilio Pratavieira", "Aracy I", "São Carlos");
+//Endereco e1 = new Endereco("Rua dos bobos, nº 0", "Engracinho", "São Juju"");
 //p1.endereco= e1;
 // Isso é:
 // O objeto completo "e1//Endereco um" foi adicionado dentro do objeto "pessoa um" que está acessando a classe endereco.
